730912b83106a47565a9d4ee6243a4fae6cc3958
Ritik Chaurasiya <121175117+Ritikchaurasiya@users.noreply.github.com>
2024-12-15 15:06:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ // popup script start from here
+ let pop = document.getElementById("popkro");
+ function popopen() {
+   pop.classList.add("formpop");
+ }
+ function popclose() {
+   pop.classList.remove("formpop");
+ }
+ // popup script end here
+
